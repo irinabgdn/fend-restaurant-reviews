5cc23a3687129fc00fe52017f01b4ab939af84e2
Irina Bogdan <irina.bogdan1@gmail.com>
2018-07-25 21:07:11 +0300

    Set responsive styles

--- js/main.js
+++ js/main.js
@@ -14,6 +14,22 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 /**
+ * Initialize map, called from HTML.
+ */
+window.initMap = () => {
+  let loc = {
+    lat: 40.722216,
+    lng: -73.987501
+  };
+  self.map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: loc,
+    scrollwheel: false
+  });
+
+  updateRestaurants();
+}
+/**
  * Fetch all neighborhoods and set their HTML.
  */
 fetchNeighborhoods = () => {
@@ -68,22 +84,6 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
   });
 }
 
-/**
- * Initialize leaflet map, called from HTML.
- */
-window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-
-  updateRestaurants();
-}
 
 /**
  * Update page and map for current restaurants.
